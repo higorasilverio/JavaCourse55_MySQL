a56da3fec81e0e965552eaa1d4e850c5f1d2ddf4
higorasilverio <higorasilverio@outlook.com>
2020-04-30 07:43:13 -0300

    Updating a database

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,41 +14,24 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 		
 		try {
 			conn = DB.getConnection();
 			
-			/*st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.00);
-			st.setInt(5, 4);*/
-			
 			st = conn.prepareStatement(
-					"insert into department (Name) values ('D1'), ('D2')",
-					Statement.RETURN_GENERATED_KEYS);
+					"UPDATE seller "
+					+ " SET BaseSalary = BaseSalary + ? "
+					+ "WHERE  "
+					+ "(DepartmentId = ?)");
+			
+			st.setDouble(1, 200.00);
+			st.setInt(2, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affecte!");
-			}
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -57,7 +40,6 @@ public class Program {
 			DB.closeStatement(st);
 			DB.closeConnectio();
 		}
-		
 		
 	}
 
